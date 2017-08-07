95fdc22e931ee0c59a268dfd3d258d74d775beaa
lordofkhaos <30538145+lordofkhaos@users.noreply.github.com>
2017-08-07 13:00:35 -0400

    Update responsive.js

--- home/assets/js/responsive.js
+++ home/assets/js/responsive.js
@@ -1,6 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementByClass("navbar");
+    var x = document.getElementByID(navbar);
     if (x.id === "topnav") {
         x.className += " responsive";
     } else {
