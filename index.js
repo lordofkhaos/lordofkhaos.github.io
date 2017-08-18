c101e7d60888d05c69f03db9fa5b5df6468fa55a
lordofkhaos <30538145+lordofkhaos@users.noreply.github.com>
2017-08-18 18:08:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
