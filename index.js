c33cf2ead50cc50da309f9db56ef4a8f12126ff5
lordofkhaos <30538145+lordofkhaos@users.noreply.github.com>
2017-08-18 18:05:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
